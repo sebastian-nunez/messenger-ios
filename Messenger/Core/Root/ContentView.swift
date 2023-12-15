1ee5dd1634c834ee34b6ea632f273aa34743ae73
sebastian-nunez <02snunez@gmail.com>
2023-12-14 22:38:04 -0500

    Add initial project files

---
+++ Messenger/Core/Root/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Messenger
+//
+//  Created by Sebastian on 12/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
