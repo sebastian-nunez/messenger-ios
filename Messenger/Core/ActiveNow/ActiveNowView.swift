f11a863bf2d9ab8361bf22718e2446d571eac11d
sebastian-nunez <02snunez@gmail.com>
2023-12-16 11:34:04 -0500

    Add ActiveNowView to InboxView

---
+++ Messenger/Core/ActiveNow/ActiveNowView.swift
@@ -0,0 +1,53 @@
+//
+//  ActiveNowView.swift
+//  Messenger
+//
+//  Created by Sebastian on 12/16/23.
+//
+
+import SwiftUI
+
+struct ActiveNowView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 32) {
+                // render users
+                ForEach(0 ... 10, id: \.self) { _ in
+                    VStack {
+                        // image
+                        ZStack(alignment: .bottomTrailing) {
+                            Image(systemName: "person.circle.fill")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 64, height: 64)
+                                .foregroundStyle(Color(.systemGray5))
+
+                            // green circle
+                            ZStack {
+                                Circle()
+                                    .fill(.white)
+                                    .frame(width: 18, height: 18)
+
+                                Circle()
+                                    .fill(.green)
+                                    .frame(width: 12, height: 12)
+                            }
+                        }
+
+                        // name
+                        Text("Bruce")
+                            .font(.subheadline)
+                            .foregroundStyle(.gray)
+                    }
+                }
+            }
+            .padding()
+        }
+        .scrollTargetBehavior(.paging)
+        .frame(height: 106)
+    }
+}
+
+#Preview {
+    ActiveNowView()
+}
