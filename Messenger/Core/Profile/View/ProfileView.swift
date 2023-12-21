b7f33e7fb676044eaaa349b6dd2ed08e4e141498
sebastian-nunez <02snunez@gmail.com>
2023-12-21 06:43:17 -0500

    added the basic profile view outline (no viewModel)

---
+++ Messenger/Core/Profile/View/ProfileView.swift
@@ -0,0 +1,61 @@
+//
+//  ProfileView.swift
+//  Messenger
+//
+//  Created by Sebastian on 12/21/23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack {
+            // header
+            VStack {
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 80, height: 80)
+                    .foregroundStyle(Color(.systemGray4))
+
+                Text("Bruce Wayne")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+            }
+
+            // list of settings
+            List {
+                Section {
+                    ForEach(1 ... 5, id: \.self) { _ in
+                        HStack {
+                            Image(systemName: "bell.circle.fill")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 24, height: 24)
+                                .foregroundStyle(Color(.systemPurple))
+
+                            Text("Notifications")
+                                .font(.subheadline)
+                        }
+                    }
+                }
+
+                Section {
+                    Button("Log Out") {
+                        print("DEBUG: logging out")
+                    }
+                    .tint(.red)
+
+                    Button("Delete Account") {
+                        print("DEBUG: deleting account")
+                    }
+                    .tint(.red)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfileView()
+}
