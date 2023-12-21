5f7ac9410eccc80e0868056c4b99ae418b32604c
sebastian-nunez <02snunez@gmail.com>
2023-12-21 06:54:59 -0500

    added the settings enum/viewmodel to manage each settings option

---
+++ Messenger/Core/Profile/ViewModel/SettingsOptionsViewModel.swift
@@ -0,0 +1,51 @@
+//
+//  SettingsOptionsViewModel.swift
+//  Messenger
+//
+//  Created by Sebastian on 12/21/23.
+//
+
+import Foundation
+import SwiftUI
+
+enum SettingsOptionsViewModel: Int, Identifiable, CaseIterable {
+    case darkMode
+    case activeStatus
+    case accessibility
+    case privacy
+    case notifications
+
+    var id: Int {
+        self.rawValue
+    }
+
+    var title: String {
+        switch self {
+        case .darkMode: "Dark Mode"
+        case .activeStatus: "Active Status"
+        case .accessibility: "Accessibility"
+        case .privacy: "Privacy and Safety"
+        case .notifications: "Notifications"
+        }
+    }
+
+    var color: Color {
+        switch self {
+        case .darkMode: .black
+        case .activeStatus: Color(.systemGreen)
+        case .accessibility: .black
+        case .privacy: Color(.systemBlue)
+        case .notifications: Color(.systemPurple)
+        }
+    }
+
+    var image: String {
+        switch self {
+        case .darkMode: "moon.circle.fill"
+        case .activeStatus: "message.badge.circle.fill"
+        case .accessibility: "person.circle.fill"
+        case .privacy: "lock.circle.fill"
+        case .notifications: "bell.circle.fill"
+        }
+    }
+}
