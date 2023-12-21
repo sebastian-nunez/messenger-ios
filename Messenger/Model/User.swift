5bcb79ab9a1f18a0ec77d18578e0d2a77864431d
sebastian-nunez <02snunez@gmail.com>
2023-12-21 07:53:42 -0500

    created user model

---
+++ Messenger/Model/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  Messenger
+//
+//  Created by Sebastian on 12/21/23.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    var id = NSUUID().uuidString
+
+    let fullName: String
+    let email: String
+    var profileImageUrl: String?
+}
+
+extension User {
+    static let MOCK_USER = User(fullName: "Bruce Wayne", email: "batman@gmail.com", profileImageUrl: "batman")
+}
