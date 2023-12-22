104321d135d8bf66b236a9cdf940a704c98d6df6
sebastian-nunez <02snunez@gmail.com>
2023-12-22 07:52:04 -0500

    added firebase setup

--- Messenger/App/MessengerApp.swift
+++ Messenger/App/MessengerApp.swift
@@ -5,10 +5,24 @@
 //  Created by Sebastian on 12/14/23.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool
+    {
+        FirebaseApp.configure()
+
+        return true
+    }
+}
 
 @main
 struct MessengerApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
