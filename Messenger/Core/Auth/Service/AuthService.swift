9a74287b26cb56d9e378582d42314b7dcfd492ef
sebastian-nunez <02snunez@gmail.com>
2023-12-27 07:32:58 -0500

    Add Firebase import and userSession property

--- Messenger/Core/Auth/Service/AuthService.swift
+++ Messenger/Core/Auth/Service/AuthService.swift
@@ -5,9 +5,12 @@
 //  Created by Sebastian on 12/22/23.
 //
 
+import Firebase
 import Foundation
 
 protocol AuthService {
+    var userSession: Firebase.User? { get }
+
     func login(withEmail email: String, password: String) async throws
 
     func createUser(withEmail email: String, password: String, fullname: String) async throws
