1b7ac4aee6c12191ea265b8268b4adf6eecf9a86
sebastian-nunez <02snunez@gmail.com>
2023-12-31 09:35:18 -0500

    Add message observation in ChatViewModel

--- Messenger/Core/Chat/ViewModel/ChatViewModel.swift
+++ Messenger/Core/Chat/ViewModel/ChatViewModel.swift
@@ -9,15 +9,24 @@ import Foundation
 
 class ChatViewModel: ObservableObject {
     @Published var messageText = ""
+    @Published var messages = [Message]()
     let user: User
 
     init(user: User) {
         self.user = user
+        observeMessages()
     }
 
     func sendMessage() {
         print("DEBUG: attempting to send message to \(user.id)")
 
         MessageServiceImpl.shared.sendMessage(to: user, messageText)
+    }
+
+    func observeMessages() {
+        MessageServiceImpl.shared.observeMessages(chatPartner: user) { messages in
+            // add the new messages
+            self.messages.append(contentsOf: messages)
+        }
     }
 }
