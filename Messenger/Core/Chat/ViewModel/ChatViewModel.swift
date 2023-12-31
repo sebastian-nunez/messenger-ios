bff45a818f67a0ac29985e8968e1947dd0652a20
sebastian-nunez <02snunez@gmail.com>
2023-12-31 08:09:05 -0500

    implemented the sendMessage within the ChatViewModel

---
+++ Messenger/Core/Chat/ViewModel/ChatViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  ChatViewModel.swift
+//  Messenger
+//
+//  Created by Sebastian on 12/31/23.
+//
+
+import Foundation
+
+class ChatViewModel: ObservableObject {
+    @Published var messageText = ""
+    let user: User
+
+    init(user: User) {
+        self.user = user
+    }
+
+    func sendMessage() {
+        print("DEBUG: attempting to send message to \(user.id)")
+
+        MessageServiceImpl.sendMessage(to: user, messageText)
+    }
+}
